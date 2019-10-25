b7103b02bcda2f4c3edf560fbf1efe5b99f4c4bf
KLI <v.developers.team+kli@gmail.com>
2019-10-25 16:02:07 +0200

    Add custom log file

--- deployer/task/deploy:extend_log.php
+++ deployer/task/deploy:extend_log.php
@@ -8,10 +8,8 @@ desc('Add custom information to the release log file');
 task('deploy:extend_log', function () {
     cd('{{deploy_path}}');
     $csv = run('tail -n 1 .dep/releases');
-    if ($csv) {
-        $metainfo = Csv::parse($csv)[0];
-        $metainfo[2] = get('branch') ?: 'master';
-        $metainfo[3] = runLocally('git config user.name')->toString();
-        run(sprintf('sed -i -e \'s/%s/%s/g\' .dep/releases', $csv, implode(',', $metainfo)));
-    }
+    $metainfo = Csv::parse($csv)[0];
+    $metainfo[2] = get('branch') ?: 'master';
+    $metainfo[3] = runLocally('git config user.name')->toString();
+    run("echo '".implode(',', $metainfo)."' >> .dep/branch");
 });
